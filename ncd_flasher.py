db5d6290126d78e766492e993b076eed5acba0ea
travis@controlanything.com <travis@controlanything.com>
2019-10-23 14:12:49 -0500

    Added bootloader and partitions to flash script

--- ncd_flasher.py
+++ ncd_flasher.py
@@ -128,13 +128,21 @@ print(firmware.get('spiffs'))
 spiffs_file = urllib.request.urlretrieve(str(firmware.get('spiffs')), './spiffs.bin')
 print(spiffs_file)
 
+print(firmware.get('bootloader'))
+bootloader_file = urllib.request.urlretrieve(str(firmware.get('bootloader')), './bootloader.bin')
+print(bootloader_file)
+
+print(firmware.get('partitions'))
+partitions_file = urllib.request.urlretrieve(str(firmware.get('partitions')), './partitions.bin')
+print(partitions_file)
+
 print(port_array.get(target_port_key))
 
 print('')
 print('fingers crossed:')
 
 try:
-    espmodule = esptool.main(['--chip', 'esp32', '--port', port_array.get(target_port_key), '--baud', '921600', '--before', 'default_reset', '--after', 'hard_reset', 'write_flash', '-z', '--flash_mode', 'dio', '--flash_size', 'detect', '2691072', 'spiffs.bin', '0x10000', 'firmware.bin'])
+    espmodule = esptool.main(['--chip', 'esp32', '--port', port_array.get(target_port_key), '--baud', '921600', '--before', 'default_reset', '--after', 'hard_reset', 'write_flash', '-z', '--flash_mode', 'dio', '--flash_freq', '40m', '--flash_size', 'detect', '0x1000', 'bootloader.bin', '0x8000', 'partitions.bin', '2691072', 'spiffs.bin', '0x10000', 'firmware.bin'])
     # espmodule = esptool.main(['--chip', 'esp32', '--port', '/dev/cu.SLAB_USBtoUART', '--baud', '921600', '--before', 'default_reset', '--after', 'hard_reset', 'write_flash', '-z', '--flash_mode', 'dio', '--flash_size', 'detect', '2691072', 'spiffs.bin', '0x10000', 'firmware.bin'])
 except:
     print('fail cu')
